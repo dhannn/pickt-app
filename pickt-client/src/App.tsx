6ad366b56433a92c4c5aa810fea0fc147e25bd1c
Daniel Ramos <daniel_ramos@dlsu.edu.ph>
2023-06-19 11:23:03 +0800

    Add Avatar component

--- pickt-client/src/App.tsx
+++ pickt-client/src/App.tsx
@@ -1,13 +1,24 @@
 import React from "react";
-import { CreatePostForm } from "./components/Forms";
-import { Vote } from "./components/shared/Button/Vote";
+import { VoteComponent } from "./components/shared/Vote/VoteComponent";
+import Avatar from "./components/shared/Avatar/Avatar";
 
 export function App() {
     return (
     <>
         {/* <CreatePostForm isFormFocused={true}></CreatePostForm> */}
-        <Vote upvotes={10} downvotes={1}/> <br/>
-        <Vote upvotes={20} downvotes={3}/>
+        <div style={{alignItems: 'center', display: "flex", flexDirection: 'column'}}>
+            <br/>
+            <br/>
+            <Avatar size='small'/>
+            <br/>
+            <br/>
+            <Avatar size='medium' url='https://images.unsplash.com/photo-1484515991647-c5760fcecfc7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'/>
+            <br/>
+            <br/>
+            <Avatar size='large' url='https://images.unsplash.com/photo-1484515991647-c5760fcecfc7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'/>
+        </div>
+        <VoteComponent upvotes={10} downvotes={1}/> <br/>
+        <VoteComponent upvotes={20} downvotes={3}/>
     </>
     );
 }
---
+++ pickt-client/src/components/shared/Avatar/Avatar.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './Avatar.module.css'
+
+type AvatarSize = 'small' | 'medium' | 'large'
+
+type AvatarProps = {
+    size: AvatarSize,
+    url?: string
+}
+
+export default function Avatar(props: AvatarProps) {
+    const {size, url} = props;
+    const avatarStyle = `${styles.avatar} ${styles[size]}`;
+    const defaultProfile = 'default_picture.png';
+
+    return (
+        <div className={avatarStyle} style={{backgroundImage: `url(${url? url: defaultProfile})`}}/>
+    );
+}
